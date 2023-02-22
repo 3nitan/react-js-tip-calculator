d5a52dc81150f88616582986ef6e9b232550d078
Abimbola <mustaphaabimbola4@gmail.com>
2023-02-22 01:15:33 +0100

    logical funtionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import './App.css';
+import { useState } from 'react';
 // import Header from './components/header';
 
 function App() {
+  const [bill, setBill] = useState(0);
+  const [tipPercentage, setTipPercentage] = useState(0);
+  const [numOfPeople, setNumOfPeople] = useState(0);
+
+  const handleBillChange = (e) => {
+    setBill(parseFloat(e.target.value));
+  };
+
+  const handleTipPercentageChange = (percentage) => {
+    setTipPercentage(percentage);
+  };
+
+  const handleNumOfPeopleChange = (e) => {
+    setNumOfPeople(parseInt(e.target.value));
+  };
+
+  const calculateTipAmount = () => {
+    if (numOfPeople === 0) {
+      return 0;
+    }
+    const tipAmount = (bill * (tipPercentage / 100)) / numOfPeople;
+    return tipAmount.toFixed(2);
+  };
+
+  const calculateTotalAmount = () => {
+    const totalAmount = bill + bill * (tipPercentage / 100);
+    return totalAmount.toFixed(2);
+  };
+
   return (
     <div className='App'>
       <header>
@@ -14,19 +44,54 @@ function App() {
             <label>Bill</label>
             <br />
             <img src='/assets/icon-dollar.svg' alt='dollar icon' />
-            <input type='number' placeholder='0.00 ' />
+            <input
+              type='number'
+              placeholder='0.00 '
+              value={bill}
+              onChange={handleBillChange}
+            />
           </div>
 
           <div className='form-tip'>
             <label>Select Tip %</label>
             <br />
             <div className='btn-container'>
-              <button className='tip-btn'>5%</button>
-              <button className='tip-btn'>10%</button>
-              <button className='tip-btn'>15%</button>
-              <button className='tip-btn'>25%</button>
-              <button className='tip-btn'>50%</button>
-              <button className='custom-tip'>Custom</button>
+              <button
+                className='tip-btn'
+                onClick={() => handleTipPercentageChange(5)}
+              >
+                5%
+              </button>
+              <button
+                className='tip-btn'
+                onClick={() => handleTipPercentageChange(10)}
+              >
+                10%
+              </button>
+              <button
+                className='tip-btn'
+                onClick={() => handleTipPercentageChange(15)}
+              >
+                15%
+              </button>
+              <button
+                className='tip-btn'
+                onClick={() => handleTipPercentageChange(25)}
+              >
+                25%
+              </button>
+              <button
+                className='tip-btn'
+                onClick={() => handleTipPercentageChange(50)}
+              >
+                50%
+              </button>
+              <button
+                className='custom-tip'
+                onClick={() => setTipPercentage(0)}
+              >
+                Custom
+              </button>
             </div>
           </div>
 
@@ -34,7 +99,13 @@ function App() {
             <label>Number of People</label>
             <br />
             <img src='/assets/icon-person.svg' alt='' />
-            <input type='number' placeholder='0 ' required />
+            <input
+              type='number'
+              placeholder='0 '
+              value={numOfPeople}
+              onChange={handleNumOfPeopleChange}
+              required
+            />
           </div>
         </form>
         <div className='output-container'>
@@ -46,7 +117,7 @@ function App() {
                 <span className='output-subtitle'>/person</span>
               </p>
 
-              <p className='output-value'>$0.00</p>
+              <p className='output-value'>${calculateTipAmount()}</p>
             </div>
 
             <div className='output'>
@@ -55,10 +126,19 @@ function App() {
                 <br />
                 <span className='output-subtitle'>/person</span>
               </p>
-              <p className='output-value'>$0.00</p>
+              <p className='output-value'>${calculateTotalAmount()}</p>
             </div>
 
-            <button className='reset-btn'>RESET</button>
+            <button
+              className='reset-btn'
+              onClick={() => {
+                setBill(0);
+                setTipPercentage(0);
+                setNumOfPeople(0);
+              }}
+            >
+              RESET
+            </button>
           </div>
         </div>
       </div>
